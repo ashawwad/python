cabef21c21884393a1f0a729b3815ce83aa72bae
Ashraf Awwad <ashrafa@ca.ibm.com>
2018-10-04 17:32:24 -0300

    Pass find character test

--- search_words.py
+++ search_words.py
@@ -5,11 +5,26 @@ class search_words:
 	def __init__(self, file_name):
 		self.file = file_name
 
-	def get_words_grid_from_file(self):
+	def read_file(self):
 		with open(self.file) as file:
 			words_data = csv.reader(file, delimiter=",")
 			words=[]
 			for word_data in words_data:
 				words.append(word_data)
-		#[0] for taking the words from first row
+		return words
+
+	def get_words_grid_from_file(self):
+		words = self.read_file()
 		return words[0] 
+
+	def get_characters_grid_from_file(self):
+		words = self.read_file()
+		return words[1:]
+
+	def find_character(self, character):
+		characters_grid = self.get_characters_grid_from_file()
+		for row in characters_grid:
+			for grid_character in row:
+				if character == grid_character:
+					return True
+		return False
