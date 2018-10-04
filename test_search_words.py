3ac1daa80e726d7a8da79ff0b13379f97528fbab
Ashraf Awwad <ashrafa@ca.ibm.com>
2018-10-04 17:19:42 -0300

    Added find character in file test

--- test_search_words.py
+++ test_search_words.py
@@ -7,10 +7,13 @@ class test_search_words(unittest.TestCase):
 
 	def test_reading_words_from_file(self):
 		self.assertEqual(7, len(self.search_class.get_words_grid_from_file()))
-		
+
 	def test_list_words_from_file(self):
 		self.assertEqual(['BONES', 'KHAN', 'KIRK', 'SCOTTY', 'SPOCK', 'SULU', 'UHURA'],
 			self.search_class.get_words_grid_from_file())
+
+	def test_find_character_in_file(self):
+		self.assertTrue(self.search_class.find_character('B'))
 		
 if __name__ == '__main__':
 	unittest.main()
