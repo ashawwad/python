f103072478f261a8c5f00f401a549d2be26297d8
Ashraf Awwad <ashrafa@ca.ibm.com>
2018-10-04 18:31:35 -0300

    Pass find word location test

--- search_words.py
+++ search_words.py
@@ -49,6 +49,26 @@ class search_words:
 
 		return False
 
+	def search_word_location(self, word, characters_grid, y,z):
+		char_range = range(len(word))
+		grid_dimension = len(characters_grid)
+		points_in_8directions = [	
+						[(y-i,z) for i in char_range if y-i>= 0],#vertical up
+						[(y+i,z) for i in char_range if y+i <= grid_dimension-1],#vertical down
+						[(y,z-i) for i in char_range if z-i >= 0], #horizontal right
+						[(y,z+i) for i in char_range if z+i <= grid_dimension-1],#horizontal left
+						[(y-i,z-i) for i in char_range if y-i>= 0 and z-i >= 0],#diagonal up left
+						[(y-i,z+i) for i in char_range if y-i >= 0 and z+i <= grid_dimension-1],#diagonal up right
+						[(y+i,z-i) for i in char_range if y+i <= grid_dimension-1 and z-i >= 0],#diagonal down left
+						[(y+i,z+i) for i in char_range if y+i <= grid_dimension-1 and z+i <= grid_dimension-1]#diagonal down right
+					 ]
+
+		for direction in points_in_8directions:
+			if word == ''.join([characters_grid[row][col] for row,col in direction]):
+				return direction
+
+		return []
+
 	def find_word_any_direction(self, searched_word):
 		characters_grid = self.get_characters_grid_from_file()
 		words = self.get_words_grid_from_file()
@@ -60,3 +80,15 @@ class search_words:
 					if result:
 						return True
 		return False
+
+	def find_word_location(self, searched_word):
+		characters_grid = self.get_characters_grid_from_file()
+		words = self.get_words_grid_from_file()
+		grid_dimension = len(characters_grid)
+		for j in range(grid_dimension):
+			for i in range(grid_dimension):
+				if characters_grid[i][j] == searched_word[0]:
+					result = self.search_word_location(searched_word, characters_grid, i, j)
+					if result:
+						return ','.join(map(str, result))
+
