6e4ea8a9ebb33a474aad6086c90fa48cf2fa5d8a
Ashraf Awwad <ashrafa@ca.ibm.com>
2018-10-04 20:07:56 -0300

    Refactoring: Added print words to soncole

--- search_words.py
+++ search_words.py
@@ -64,8 +64,8 @@ class search_words:
 					 ]
 
 		for direction in points_in_8directions:
-			if word == ''.join([characters_grid[row][col] for row,col in direction]):
-				return direction
+			if word == ''.join([characters_grid[row][col] for row, col in direction]):
+				return [(row, col) for col, row in direction]
 
 		return []
 
@@ -85,10 +85,28 @@ class search_words:
 		characters_grid = self.get_characters_grid_from_file()
 		words = self.get_words_grid_from_file()
 		grid_dimension = len(characters_grid)
-		for j in range(grid_dimension):
-			for i in range(grid_dimension):
+		for i in range(grid_dimension):
+			for j in range(grid_dimension):
 				if characters_grid[i][j] == searched_word[0]:
 					result = self.search_word_location(searched_word, characters_grid, i, j)
 					if result:
 						return ','.join(map(str, result))
 
+	def print_file_words_with_location(self):
+		characters_grid = self.get_characters_grid_from_file()
+		words = self.get_words_grid_from_file()
+		grid_dimension = len(characters_grid)
+		results = []
+		for word in words:
+			for i in range(grid_dimension):
+				for j in range(grid_dimension):
+					if characters_grid[i][j] == word[0]:
+						result = self.search_word_location(word, characters_grid, i, j)
+						if result:
+							results.append("{0}: {1}".format(word, ','.join(map(str, result))))
+
+		print("\n".join(results))
+
+if __name__ == '__main__':
+	search_class = search_words("data.csv")
+	search_class.print_file_words_with_location()
--- test_search_words.py
+++ test_search_words.py
@@ -23,10 +23,11 @@ class test_search_words(unittest.TestCase):
 		self.assertFalse(self.search_class.find_word_any_direction('PYTHON'))
 
 	def test_find_word_location(self):
-		self.assertEqual('(6, 0),(7, 0),(8, 0),(9, 0),(10, 0)', self.search_class.find_word_location('BONES'))
-		self.assertEqual('(5, 0),(5, 1),(5, 2),(5, 3),(5, 4),(5, 5)', self.search_class.find_word_location('SCOTTY'))
-		self.assertEqual('(0, 4),(1, 3),(2, 2),(3, 1),(4, 0)', self.search_class.find_word_location('UHURA'))
+		self.assertEqual('(0, 6),(0, 7),(0, 8),(0, 9),(0, 10)', self.search_class.find_word_location('BONES'))
+		self.assertEqual('(0, 5),(1, 5),(2, 5),(3, 5),(4, 5),(5, 5)', self.search_class.find_word_location('SCOTTY'))
+		self.assertEqual('(4, 0),(3, 1),(2, 2),(1, 3),(0, 4)', self.search_class.find_word_location('UHURA'))
 		self.assertEqual(None, self.search_class.find_word_location('ASHRAF'))
 
 if __name__ == '__main__':
 	unittest.main()
+	self.search_class.print_file_words_with_location()
