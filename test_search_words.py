87dd5ecd520e4aa2be781e8890981ce7e49c2fe8
Ashraf Awwad <ashrafa@ca.ibm.com>
2018-10-04 16:55:35 -0300

    Added more test

--- test_search_words.py
+++ test_search_words.py
@@ -8,5 +8,9 @@ class test_search_words(unittest.TestCase):
 	def test_reading_words_from_file(self):
 		self.assertEqual(7, len(self.search_class.get_words_grid_from_file()))
 		
+	def test_list_words_from_file(self):
+		self.assertEqual(['BONES', 'KHAN', 'KIRK', 'SCOTTY', 'SPOCK', 'SULU', 'UHURA'],
+			self.search_class.get_words_grid_from_file())
+		
 if __name__ == '__main__':
 	unittest.main()
